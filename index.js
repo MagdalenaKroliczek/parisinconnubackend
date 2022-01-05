0aa1eda9a6d3cb07fdad97ee13894b5119f51d3c
Magdalena <magdalena.kroliczek@gmail.com>
2022-01-05 12:28:13 +0100

    bdd

--- index.js
+++ index.js
@@ -1,8 +1,112 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+require("dotenv").config();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+ const images = [
+    {  
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304363/DSC_0217_xqppm1.jpg" },
+    {
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304379/DSC_0872_amkfeq.jpg",
+    },
+    {
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304335/abraxasCam_zeafil.jpg",
+    },
+    
+  ];
+  
+ const images1 =[
+    {  img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304368/domaine1_ggjuz6.jpg" },
+    {
+      
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304351/domaine2_l6dzsa.jpg",
+    },
+    {
+      
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304358/domaine3_jpi17a.jpg",
+    },
+  ];
+  
+ const images3 = [
+    {  img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304334/cementery1_tdgvtj.jpg" },
+    {
+      
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304341/cementery2_rp5aqk.jpg",
+    },
+    {
+      
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304330/cementery3_dc4ftj.jpg",
+    },
+  ];
+  
+ const images4 =[
+    {  img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304399/epernon1_t8sui7.jpg" },
+    {
+      
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304399/epernon2_ndywgc.jpg",
+    },
+    {
+      
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304401/epernon3_zymhk3.jpg",
+    },
+  ];
+  
+  const images5 =[
+    {  img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304382/hopital3_i4ktlh.jpg" },
+    {
+      
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304501/DSC_0260_l4dm6p.jpg",
+    },
+    {
+      
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304384/hopital4_q4wcpp.jpg",
+    },
+  ];
+  
+ const images6 = [
+    {  img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304423/senchal1_vv2ibu.jpg" },
+    {
+      
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304426/senchal2_nozert.jpg",
+    },
+    {
+      
+      img: "https://res.cloudinary.com/magdakroliczek/image/upload/v1641304442/senchal3_vcfimy.jpg",
+    },
+  ];
 
-app.listen(3000)
+app.get("/images", function (req, res) {
+  console.log("appel data yannick");
+  res.json(images);
+});
+
+app.get("/images1", function (req, res) {
+    console.log("appel data yannick");
+    res.json(images1);
+  });
+
+  app.get("/images3", function (req, res) {
+    console.log("appel data yannick");
+    res.json(images3);
+  });
+
+  app.get("/images4", function (req, res) {
+    console.log("appel data yannick");
+    res.json(images4);
+  });
+
+  app.get("/images5", function (req, res) {
+    console.log("appel data yannick");
+    res.json(images5);
+  });
+
+  app.get("/images6", function (req, res) {
+    console.log("appel data yannick");
+    res.json(images6);
+  });
+
+app.get("/", function (req, res) {
+  console.log("appel Home");
+  res.json("HELLO");
+});
+
+app.listen(process.env.PORT);
