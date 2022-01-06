c3a4d04c52221eba3db42c0d51c1a8cca8e50779
Magdalena <magdalena.kroliczek@gmail.com>
2022-01-06 16:33:58 +0100

    first

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(process.env.PORT);
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "magdalena.kroliczek@gmail.com",
+    pass: "Romain1988",
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
 
  const images = [
     {  
@@ -109,4 +133,26 @@ app.get("/", function (req, res) {
   res.json("HELLO");
 });
 
-app.listen(process.env.PORT);
+// app.listen(process.env.PORT);
+
+
+router.post("/contactform", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message; 
+  const mail = {
+    from: name,
+    to: "magdalena.kroliczek@gmail.com",
+    subject: "Contact Form Submission",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR" });
+    } else {
+      res.json({ status: "Message Sent" });
+    }
+  });
+});
